ebbda38ab2b8b73b302975356cf4a937f9da62f1
aryan <47309918+winexe0@users.noreply.github.com>
2020-07-23 17:21:57 -0700

    Update clock.cpp

--- clock.cpp
+++ clock.cpp
@@ -9,5 +9,6 @@ int main()
 
 	tm *tm_local = localtime(&curr_time);
 	cout << "The time is now : " << tm_local->tm_hour << ":" << tm_local->tm_min << ":" << tm_local->tm_sec;
+	system("PAUSE");
 	return 0;
 }
